6e620e221e732c1e3ddb0beedddf96ffc690980f
jai-dewani <jai.dewani.99@gmail.com>
2019-05-15 11:22:54 +0530

    Update round 558/B

--- CodeForces/round558/B.py
+++ CodeForces/round558/B.py
@@ -1,26 +1,38 @@
 from collections import Counter
+
 n = int(input())
 ar = [int(x) for x in input().split()]
-
-arr = [0 for i in range(11)]
-
-more_than_zero = 0
-unequal = 0
+hash = [0 for i in range(11)]
+temp_ar = []
 answer = 0
-for i in range(len(ar)):
-	if arr[ar[i]]==0:
-		more_than_zero += 1
-	arr[ar[i]] += 1
-	dic = Counter(arr)
-	dic.pop(0)
-
-	count = list(dic.keys())
-	
-	if(len(count)==2 and (count[0]==1 or count[1]==1)):
-		answer = i+1
-		print("1",arr,dic,count)
-		# unequal = 2
-	elif(len(count)==1):
-		answer = i+1
-		print("2",arr,dic,count)
+for i in range(n):
+    hash[ar[i]] += 1
+    dic = Counter(hash)
+    dic.pop(0)
+    keys = list(dic.keys())
+    values = list(dic.values())
+    
+    if len(keys)==2:
+        if values[0]==1 or values[1]==1:
+            if values[0]==1 and keys[0]==1:
+                answer = i+1
+                print(i,hash,dic,keys,values)
+            elif values[1]==1 and keys[1]==1:
+                answer = i+1
+                print(i,hash,dic,keys,values)
+        
+        if keys[0]-keys[1]==1 and values[0]==1:
+            answer = i+1
+            print("5",i,hash,dic,keys,values)
+        elif keys[1]-key[0]==1 and values[1]==1:
+            answer = i+1
+        '''
+        if temp_ar[0]==1 or temp_ar[1]==1:
+            answer = i+1
+        elif (temp_ar[0]-temp_ar[1])==1:
+            answer = i+1
+        ''' 
+    elif len(keys)==1 and keys[0]==1:
+        answer = i+1
+        
 print(answer)
