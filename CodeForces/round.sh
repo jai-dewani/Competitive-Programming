a2006e53b671ba56d4b0a20dd81fd0e21d0b0806
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2020-09-24 21:52:28 +0530

    End line problem

--- CodeForces/round.sh
+++ CodeForces/round.sh
@@ -1,26 +1,18 @@
 echo -n "Round Name:"
 read round
 mkdir $round 
+
+template="for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    n,m = map(int,input().split())
+    ar = list(map(int,input().split()))"
+
 touch $round/A.py
 touch $round/B.py
 touch $round/C.py
-echo "for _ in range(int(input())):
-    n = int(input())
-    s = input()
-    n,m = map(int,input().split())
-    ar = list(map(int,input().split()))
-" > $round/A.py
-echo "for _ in range(int(input())):
-    n = int(input())
-    s = input()
-    n,m = map(int,input().split())
-    ar = list(map(int,input().split()))
-" > $round/B.py
-echo "for _ in range(int(input())):
-    n = int(input())
-    s = input()
-    n,m = map(int,input().split())
-    ar = list(map(int,input().split()))
-" > $round/C.py
+echo "${template}" > $round/A.py
+echo "${template}" > $round/B.py
+echo "${template}" > $round/C.py
 
 code $round/.
